8990a4f81d998ab6947ce5a161344030bd3630ca
Charles Harper <chuckharper1969@gmail.com>
2023-08-21 23:29:37 -0500

    readme

--- cribl/cribl_controller/sync_portal_destinations/main.py
+++ cribl/cribl_controller/sync_portal_destinations/main.py
@@ -2,8 +2,15 @@ import os
 import json
 import requests
 import base64
+import logging
+import sys
+from splunk_hec_handler import SplunkHecHandler
 
 requests.urllib3.disable_warnings(requests.urllib3.exceptions.InsecureRequestWarning)
+
+logger = logging.getLogger("sync_portal_destinations")
+logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
+logger.setLevel(logging.DEBUG)
 
 ##############################################################################
 # Load config file
@@ -25,8 +32,8 @@ def load_json_file(file_path):
 def isBase64(sb):
     try:
         if isinstance(sb, str):
-            # If there's any unicode here, an exception will be thrown and the function will return false
-            sb_bytes = bytes(sb, 'ascii')
+            # If there"s any unicode here, an exception will be thrown and the function will return false
+            sb_bytes = bytes(sb, "ascii")
         elif isinstance(sb, bytes):
             sb_bytes = sb
         else:
@@ -92,8 +99,8 @@ def portal_auth(portal_connection, verify=False):
 def portal_destinations_get(portal_connection, portal_type):
 
     header = {
-        'Accept': 'application/json', 
-        'Authorization': f'Token {portal_connection["token"]}'
+        "Accept": "application/json", 
+        "Authorization": f"Token {portal_connection['token']}"
     }
 
     endpoint = f"{portal_connection['url']}/api/v1/{portal_type}/get/"
@@ -126,7 +133,7 @@ def auth(cribl_connection, verify=False):
         r.raise_for_status()
     except requests.exceptions.RequestException as e:
         raise SystemExit(str(e))
-
+    
     return r.json()["token"]
 
 
@@ -137,7 +144,7 @@ def cribl_get_outputs(cribl_connection):
 
     header = {
         "Accept": "application/json", 
-        "Authorization": f"Bearer {cribl_connection['token']}"
+        "Authorization": f"Bearer {cribl_connection['token'] }"
     }
 
     endpoint = f"{cribl_connection['url']}/api/v1/m/{cribl_connection['group']}/system/outputs"
@@ -155,12 +162,12 @@ def cribl_get_outputs(cribl_connection):
 ##############################################################################
 def cribl_update_destination(cribl_connection, output_id, json_output):
     headers = {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'Authorization': f'Bearer {cribl_connection["token"]}'
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {cribl_connection['token'] }"
     }
 
-    endpoint = f'{cribl_connection["url"]}/api/v1/m/{cribl_connection["group"]}/system/outputs/{output_id}'
+    endpoint = f"{cribl_connection['url']}/api/v1/m/{cribl_connection['group']}/system/outputs/{output_id}"
 
     try:
         r = requests.patch(endpoint, headers=headers, json=json_output, verify=False)
@@ -175,11 +182,11 @@ def cribl_update_destination(cribl_connection, output_id, json_output):
 ##############################################################################
 def cribl_delete_destination(cribl_connection, output_id):
     headers = {
-        'Accept': 'application/json', 
-        'Authorization': f'Bearer {cribl_connection["token"]}'
+        "Accept": "application/json", 
+        "Authorization": f"Bearer {cribl_connection['token'] }"
     }
 
-    endpoint = f'{cribl_connection["url"]}/api/v1/m/{cribl_connection["group"]}/system/outputs/{output_id}'
+    endpoint = f"{cribl_connection['url']}/api/v1/m/{cribl_connection['group']}/system/outputs/{output_id}"
 
     try:
         r = requests.delete(endpoint, headers=headers, verify=False)
@@ -195,11 +202,11 @@ def cribl_delete_destination(cribl_connection, output_id):
 def cribl_add_destination(cribl_connection, json_new_output):
     print(json_new_output)
     headers = {
-        'Accept': 'application/json', 
-        'Authorization': 'Bearer ' + cribl_connection['token'] 
+        "Accept": "application/json", 
+        "Authorization": f"Bearer {cribl_connection['token'] }"
     }
 
-    endpoint = f'{cribl_connection["url"]}/api/v1/m/{cribl_connection["group"]}/system/outputs'
+    endpoint = f"{cribl_connection['url']}/api/v1/m/{cribl_connection['group']}/system/outputs"
 
     try:
         r = requests.post(endpoint, headers=headers, json=json_new_output, verify=False)
@@ -215,66 +222,87 @@ def cribl_add_destination(cribl_connection, json_new_output):
 # -Add any that do not exist; 
 # -Remove any that exist that are no longer in portal
 # -Update any notable fields that have changed in portal
-# -cribl destinations that are involved in compare are distinguishable through streamtag of 'criblauto-portal'
-# - if this is not acceptable than an 'automation prefix' can be applied to output_id
+# -cribl destinations that are involved in compare are distinguishable through streamtag of "criblauto-portal"
+# - if this is not acceptable than an "automation prefix" can be applied to output_id
 ##############################################################################
 def main():
+    
     cwd = os.path.dirname(os.path.realpath(__file__))
-    config_file = os.path.join(cwd, 'config.json')
-    config_json = load_json_file(config_file)
+    config_file = os.path.join(cwd, "config.json")
+    try:
+        config_json = load_json_file(config_file)
+    except:
+        raise SystemExit(f"Unable to read json file '{config_file}'")
 
-    secret_json_file = "C:\\Users\\email\\secret.json"
-    credentials = process_authentication_file(secret_json_file)
+    try:
+        secret_json_file = config_json["auth"]["secret_file_location"]
+        credentials = process_authentication_file(secret_json_file)
+    except:
+        raise SystemExit(f"Unable to process authentication file '{secret_json_file}'")
 
+    logger.setLevel(logging.DEBUG)
+    splunk_handler = SplunkHecHandler(config_json["logging"]["hec"]["host"],
+                        credentials["development"]["splunk_hec"]["token"],
+                        port=config_json["logging"]["hec"]["port"], 
+                        proto=config_json["logging"]["hec"]["proto"], 
+                        ssl_verify=config_json["logging"]["hec"]["ssl_verify"],
+                        source=config_json["logging"]["hec"]["source"]
+    )
+    logger.addHandler(splunk_handler)
+    
+
+    logger.debug("Requesting cribl authorization token")
     cribl_conn = {
         "username": credentials["development"]["cribl_conn"]["username"],
         "password": credentials["development"]["cribl_conn"]["password"],
-        "url": "http://cribl.maejer.lab:9000",
-        "group": "default"
+        "url": config_json["cribl_conn"]["url"],
+        "group": config_json["cribl_conn"]["group"]
     }
     cribl_conn["token"] = auth(cribl_conn)
     
+    logger.debug("Requesting portal authorization token")
     portal_conn = {
         "username": credentials["development"]["portal_conn"]["username"],
         "password": credentials["development"]["portal_conn"]["password"],
-        "url": "http://127.0.0.1:8000"
+        "url": config_json["portal_conn"]["url"]
     }
-    
     portal_conn["token"] = portal_auth(portal_conn)
 
     portal_destinations = portal_destinations_get(portal_conn, "elastic")
-    cribl_destinations = cribl_get_outputs(cribl_conn)['items']
+    cribl_destinations = cribl_get_outputs(cribl_conn)["items"]
 
     cribl_automated = []
     for cribl_destination in cribl_destinations:
-        if not cribl_destination['type'] == 'elastic':
+        if not cribl_destination["type"] == "elastic":
             continue
         if not "criblauto-portal" in cribl_destination["streamtags"]:
             continue
         cribl_automated.append(cribl_destination)
     
     # find destinations that need to be updated
+    logger.debug("Checking if destinations need to be updated")
     for portal_destination in portal_destinations:
         for existing_destination in cribl_automated:
             if portal_destination["title"] == existing_destination["id"]:
                 # compare values
                 modified = False
-                if not existing_destination['auth']['username'] == portal_destination['username']:
-                    existing_destination['auth']['username'] = portal_destination['username']
+                if not existing_destination["auth"]["username"] == portal_destination["username"]:
+                    existing_destination["auth"]["username"] = portal_destination["username"]
                     modified = True
-                if not existing_destination['auth']['password'] == portal_destination['password']:
-                    existing_destination['auth']['password'] = portal_destination['password']
+                if not existing_destination["auth"]["password"] == portal_destination["password"]:
+                    existing_destination["auth"]["password"] = portal_destination["password"]
                     modified = True
-                if not existing_destination['url'] == portal_destination['url']:
-                    existing_destination['url'] = portal_destination['url']
+                if not existing_destination["url"] == portal_destination["url"]:
+                    existing_destination["url"] = portal_destination["url"]
                     modified = True
                 if modified == False:
                     continue
-                result = cribl_update_destination(cribl_conn, existing_destination['id'], existing_destination)
-                print(result)
+                logger.debug(f"Updating destination '{existing_destination['id']}'")
+                result = cribl_update_destination(cribl_conn, existing_destination["id"], existing_destination)
 
     
     # find destinations that need to be added
+    logger.debug("Checking if destinations need to be added")
     for portal_destination in portal_destinations:
         found = False
         for existing_destination in cribl_automated:
@@ -283,28 +311,30 @@ def main():
         if found == True:
             continue
         # Add destination to cribl
-        json_obj = config_json['templates']['elastic_output'].copy()
-        json_obj['id'] = portal_destination['title']
-        json_obj['auth']['username'] = portal_destination['username']
-        json_obj['auth']['password'] = portal_destination['password']
-        json_obj['url'] = portal_destination['url']
-        json_obj['streamtags'].append("criblauto-portal")
+        json_obj = config_json["templates"]["elastic_output"].copy()
+        json_obj["id"] = portal_destination["title"]
+        json_obj["auth"]["username"] = portal_destination["username"]
+        json_obj["auth"]["password"] = portal_destination["password"]
+        json_obj["url"] = portal_destination["url"]
+        json_obj["streamtags"].append("criblauto-portal")
         
+        logger.debug(f"Adding destination {json_obj['id']}")
         result = cribl_add_destination(cribl_conn, json_obj)
-        print(result)
     
     # find destinations that need to be removed
+    logger.debug("Checking if destinations need to be removed")
     for existing_destination in cribl_automated:
         found = False
         for portal_destination in portal_destinations:
-            if portal_destination['title'] == existing_destination['id']:
+            if portal_destination["title"] == existing_destination["id"]:
                 found = True
         if found == True:
             continue
         
-        result = cribl_delete_destination(cribl_conn, existing_destination['id'])
-        print(result)
+        logger.debug(f"Removing destination {existing_destination['id']}")
+        result = cribl_delete_destination(cribl_conn, existing_destination["id"])
     
+    logger.debug("Finished")
     
 if __name__ == "__main__":
     main()
--- cribl/cribl_controller/sync_portal_destinations/readme.md
+++ cribl/cribl_controller/sync_portal_destinations/readme.md
@@ -22,3 +22,79 @@ Requires a secret.json in the following format. The password is entered in plain
     }
 }
 ```
+
+# config.json
+```
+{
+    "cribl_conn": {
+        "url": "http://cribl.maejer.lab:9000",
+        "group": "default"
+    },
+    "portal_conn": {
+        "url": "http://127.0.0.1:8000"
+    },
+    "auth": {
+        "secret_file_location": "C:\\Users\\email\\secret.json"
+    },
+    "logging": {
+        "hec": {
+            "host": "cribl-worker01.maejer.lab",
+            "port": "8088",
+            "proto": "http",
+            "ssl_verify": false,
+            "source": "sync_portal_destinations"
+        },
+        "level": 10
+    },
+    "templates": {
+        "elastic_output_router": {
+            "systemFields": [
+                "cribl_pipe"
+            ],
+            "streamtags": [
+                "elk",
+                "elastic"
+            ],
+            "rules": [
+            {
+                "final": true,
+                "filter": "true",
+                "output": "devnull",
+                "description": "default_description"
+            }
+            ],
+            "type": "router",
+            "id": "ELK_Outputs_Router"
+        },
+        "elastic_output": {
+            "id": "###LABEL###",
+            "systemFields": [
+              "cribl_pipe"
+            ],
+            "streamtags": [],
+            "loadBalanced": false,
+            "concurrency": 5,
+            "maxPayloadSizeKB": 4096,
+            "maxPayloadEvents": 0,
+            "compress": false,
+            "rejectUnauthorized": false,
+            "timeoutSec": 30,
+            "flushPeriodSec": 1,
+            "failedRequestLoggingMode": "none",
+            "safeHeaders": [],
+            "auth": {
+              "disabled": false,
+              "authType": "manual",
+              "username": "###USERNAME###",
+              "password": "###PASSWORD###"
+            },
+            "elasticVersion": "auto",
+            "onBackpressure": "block",
+            "useRoundRobinDns": false,
+            "type": "elastic",
+            "index": "'default'",
+            "url": "###URL###"
+        }
+    }
+}   
+```
